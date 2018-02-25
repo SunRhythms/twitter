a2660e9d3b9799fb32aa914dd2ba940ffe0a4f52
Sun <Sun@Suns-MacBook-Pro.local>
2018-02-25 17:46:30 -0500

    making sure

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const hbs = require('express-handlebars');
+
+const app = express();
