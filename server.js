5f4199ce5a64da618fceee45b04709a0723e3fb5
Sun <Sun@Suns-MacBook-Pro.local>
2018-02-25 21:49:22 -0500

    adding port and appJson

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const app = express();
 
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(3030, (err) => {
+app.listen(5000, (err) => {
  if (err) console.log(err);
- console.log("Running on 3030 port");
+ console.log("Running on 5000 port");
 
 });
