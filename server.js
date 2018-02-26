a0969723df37248c05d51e143b2b4128575bf9ed
Sun <Sun@Suns-MacBook-Pro.local>
2018-02-25 19:05:40 -0500

    server js update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -5,3 +6,17 @@ const mongoose = require('mongoose');
 const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname _ '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+ if (err) console.log(err);
+ console.log("Running on env port");
+
+});
